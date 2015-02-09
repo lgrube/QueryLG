db82785ecc7dc2251e819ddfbba84d6ca3dffbb7
lgrube <lgv9483@saintjoe.edu>
2015-02-09 15:23:04 -0600

    second question confused me a bit bc there isnt any matching names

--- query4.10.sql
+++ query4.10.sql
@@ -1,11 +1,14 @@
-question 1
+/*question 1*/
 
 select fname, lname from employee as e, works_on as w, where e.ssn=w.essn and w.pno=1;
 
 
-question 2
+/*question 2*/
 
+/*this confused me a bit because there isn't any matches of first names*/
 
-question 3
+select fname from employee as e, dependent as d where d.dependent_name=e.fname
+
+/*question 3*/
 
 select fname, lname from employee where super_ssn='333445555';
